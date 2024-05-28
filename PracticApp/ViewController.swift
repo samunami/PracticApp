5abd4ac32ae3b462314fb96943439ebf9eed1d62
samunami <salamon51020@gmail.com>
2024-05-28 16:47:44 +0300

    add trafficAPP

--- PracticApp/ViewController.swift
+++ PracticApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet weak var redViwev: UIView!
+    @IBOutlet weak var orangeViwev: UIView!
+    @IBOutlet weak var greenViwev: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redViwev.layer.cornerRadius = 100
+        orangeViwev.layer.cornerRadius = 100
+        greenViwev.layer.cornerRadius = 100
+        
+        redViwev.backgroundColor = .clear
+        orangeViwev.backgroundColor = .clear
+        greenViwev.backgroundColor = .clear
     }
-
-
+ 
+    @IBAction func buttonHide(_ sender: UIButton) {
+        if redViwev.backgroundColor == .clear && orangeViwev.backgroundColor == .clear &&
+            greenViwev.backgroundColor == .clear{
+            redViwev.backgroundColor = .red
+        } else if redViwev.backgroundColor == .red  {
+            redViwev.backgroundColor = .clear
+            orangeViwev.backgroundColor = .orange
+        } else if orangeViwev.backgroundColor == .orange {
+            orangeViwev.backgroundColor = .clear
+            greenViwev.backgroundColor = .green
+        } else if greenViwev.backgroundColor == .green {
+            greenViwev.backgroundColor = .clear
+        }
+    }
+    
 }
+
 
